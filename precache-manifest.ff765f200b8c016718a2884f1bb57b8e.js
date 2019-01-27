ea60deb5934c84b6964f3ce33c345e8e38670c92
huybq0002 <huybq.uel@gmail.com>
2019-01-27 11:51:06 +0700

    Add files via upload

---
+++ precache-manifest.ff765f200b8c016718a2884f1bb57b8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "574a911078200a31b5d1",
+    "url": "/static/js/main.574a9110.chunk.js"
+  },
+  {
+    "revision": "18b239a27b05f68b0330",
+    "url": "/static/js/1.18b239a2.chunk.js"
+  },
+  {
+    "revision": "574a911078200a31b5d1",
+    "url": "/static/css/main.ec68446a.chunk.css"
+  },
+  {
+    "revision": "d79548f23992f35634d119ec6da53ef2",
+    "url": "/index.html"
+  }
+];
